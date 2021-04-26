4d7eae1ff37a83ca18dbec9034fd656e47c318cf
hillexed <hillexed@email.com>
2021-04-26 11:30:29 -0400

    Add debug mode

--- bot.py
+++ bot.py
@@ -3,6 +3,18 @@ config = dotenv.dotenv_values(".env")
 
 from game import SingleHole
 import time
+import sys
+import asyncio
+
+
+debug = False
+prefix = '!'
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "debug":
+        debug=True
+        print("Debug mode on!")
+        prefix = 'd' + prefix
 
 client = discord.Client()
 @client.event
@@ -11,11 +23,17 @@ async def on_ready():
 
 async def newglolfgame(message):
     game = SingleHole()
-    glolfgame = await message.channel.send(game.printboard())
-    for i in range(60):
-        time.sleep(3)
-        game.update()
+    glolfgame = await message.channel.send("Beginning game...")
+    await asyncio.sleep(2)
+    try:
         await glolfgame.edit(content=game.printboard())
+        for i in range(60):
+            await asyncio.sleep(3)
+            game.update()
+            await glolfgame.edit(content=game.printboard())
+    except Exception as e:
+            await glolfgame.add_reaction('⚠️')
+            raise e
 
     
     await glolfgame.edit(content=game.printboard() + '\n' + "Game over!")
@@ -24,8 +42,12 @@ async def newglolfgame(message):
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content == "!glolf":
+    if message.content == prefix + "glolf":
         print("glolf detected")
         await newglolfgame(message)
 
-client.run(config["TOKEN"])
+if debug:
+    client.run(config["DEV_TOKEN"])
+else:
+    client.run(config["TOKEN"])
+    
