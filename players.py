9b2e0fb9dc7df89fb251089a36e10a8c03a6fe55
hillexed <hillexed@email.com>
2021-04-28 16:12:49 -0400

    Modifications

--- players.py
+++ players.py
@@ -12,24 +12,24 @@ def random_player_emoji(rng):
 "🕵️‍♂️","👩‍⚕️","🧑‍⚕️","👨‍⚕️","👩‍🌾","🧑‍🌾","👨‍🌾","👩‍🍳","🧑‍🍳","👨‍🍳","👩‍🎓","🧑‍🎓","👨‍🎓","👩‍🎤",
 "🧑‍🎤","👨‍🎤","👩‍🏫","🧑‍🏫","👨‍🏫","👩‍🏭","🧑‍🏭","👨‍🏭","👩‍💻","🧑‍💻","👨‍💻","👩‍💼","🧑‍💼","👨‍💼",
 "👩‍🔧","🧑‍🔧","👨‍🔧","👩‍🔬","🧑‍🔬","👨‍🔬","👩‍🎨","🧑‍🎨","👨‍🎨","👩‍🚒","🧑‍🚒","👨‍🚒","👩‍✈️","🧑‍✈️",
-"👨‍✈️","👩‍🚀","🧑‍🚀","👨‍🚀","👩‍⚖️","🧑‍⚖️","👨‍⚖️","👰","","🤵","","👸","🤴","🦸‍♀️","🦸",
-"🦸‍♂️","🦹‍♀️","🦹","🦹‍♂️","🤶","","🎅","🧙‍♀️","🧙","🧙‍♂️","🧝‍♀️","🧝","🧝‍♂️","🧛‍♀️","🧛",
+"👨‍✈️","👩‍🚀","🧑‍🚀","👨‍🚀","👩‍⚖️","🧑‍⚖️","👨‍⚖️","👰","🏋️","🤵","🤸","👸","🤴","🦸‍♀️","🦸",
+"🦸‍♂️","🦹‍♀️","🦹","🦹‍♂️","🤶","🚴","🎅","🧙‍♀️","🧙","🧙‍♂️","🧝‍♀️","🧝","🧝‍♂️","🧛‍♀️","🧛",
 "🧛‍♂️","🧟‍♀️","🧟","🧟‍♂️","🧞‍♀️","🧞","🧞‍♂️","🧜‍♀️","🧜","🧜‍♂️","🧚‍♀️","🧚","🧚‍♂️","👼","🤰",
 "🤱","🙇‍♀️","🙇","🙇‍♂️","💁‍♀️","💁","💁‍♂️","🙅‍♀️","🙅","🙅‍♂️","🙆‍♀️","🙆","🙆‍♂️","🙋‍♀️","🙋",
 "🙋‍♂️","🧏‍♀️","🧏","🧏‍♂️","🤦‍♀️","🤦","🤦‍♂️","🤷‍♀️","🤷","🤷‍♂️","🙎‍♀️","🙎","🙎‍♂️","🙍‍♀️","🙍",
 "🙍‍♂️","💇‍♀️","💇","💇‍♂️","💆‍♀️","💆","💆‍♂️","🧖‍♀️","🧖","🧖‍♂️","💅","🤳","💃","🕺","🕴","👩‍🦽",
 "🧑‍🦽","👨‍🦽","👩‍🦼","🧑‍🦼","👨‍🦼","🚶‍♀️","🚶","🚶‍♂️","👩‍🦯","🧑‍🦯","👨‍🦯","🧎‍♀️","🧎","🧎‍♂️","🏃‍♀️","🏃",
-"🏃‍♂️","🧍‍♀️","🧍","🧍‍♂️","👭","🧑‍🤝‍🧑","👬","👫","👩‍❤️‍👩","💑","👨‍❤️‍👨","👩‍❤️‍👨","👩‍❤️‍💋‍👩","💏","👨‍❤️‍💋‍👨","👩‍❤️‍💋‍👨"
-,"👪","👨‍👩‍👦","👨‍👩‍👧","👨‍👩‍👧‍👦","👨‍👩‍👦‍👦","👨‍👩‍👧‍👧","👨‍👨‍👦","👨‍👨‍👧","👨‍👨‍👧‍👦","👨‍👨‍👦‍👦","👨‍👨‍👧‍👧","👩‍👩‍👦","👩‍👩‍👧","👩‍👩‍👧‍👦","👩‍👩‍👦‍👦","👩‍👩‍👧‍👧",
+"🏃‍♂️","🧍‍♀️","🧍","🧍‍♂️","👭","🧑‍🤝‍🧑","👬","👫","👩‍❤️‍👩","💑","👨‍❤️‍👨","👩‍❤️‍👨","👩‍❤️‍💋‍👩","💏","👨‍❤️‍💋‍👨","👩‍❤️‍💋‍👨",
+"👪","👨‍👩‍👦","👨‍👩‍👧","👨‍👩‍👧‍👦","👨‍👩‍👦‍👦","👨‍👩‍👧‍👧","👨‍👨‍👦","👨‍👨‍👧","👨‍👨‍👧‍👦","👨‍👨‍👦‍👦","👨‍👨‍👧‍👧","👩‍👩‍👦","👩‍👩‍👧","👩‍👩‍👧‍👦","👩‍👩‍👦‍👦","👩‍👩‍👧‍👧",
 "👨‍👦","👨‍👦‍👦","👨‍👧","👨‍👧‍👦","👨‍👧‍👧","👩‍👦","👩‍👦‍👦","👩‍👧","👩‍👧‍👦","👩‍👧‍👧","🗣","👤","👥"]
 
     nonhumanoid=[
 "🐶","🐱","🐭","🐹","🐰","🦊","🐻","🐼","🐨","🐯","🦁","🐮","🐷","🐽","🐸","🐵",
 "🙈","🙉","🙊","🐒","🐔","🐧","🐦","🐤","🐣","🐥","🦆","🦅","🦉","🦇","🐺","🐗","🐴","🦄",
-"🐝","🐛","🦋","🐌","🐞","🐜","🦟","🦗","🕷","🦂","🐢","🐍","🦎","🦖","🦕","🐙","🦑","🦐",
+"🐝","🐛","🦋","🐌","🐞","🐜","🦟","🦗","🕷️","🦂","🐢","🐍","🦎","🦖","🦕","🐙","🦑","🦐",
 "🦞","🦀","🐡","🐠","🐟","🐬","🐳","🐋","🦈","🐊","🐅","🐆","🦓","🦍","🦧","🐘","🦛","🦏",
 "🐪","🐫","🦒","🦘","🐃","🐂","🐄","🐎","🐖","🐏","🐑","🦙","🐐","🦌","🐕","🐩","🦮","🐕‍🦺",
-"🐈","🐓","🦃","🦚","🦜","🦢","🦩","🕊","🐇","🦝","🦨","🦡","🦦","🦥","🐁","🐀","🐿","🦔",
+"🐈","🐓","🦃","🦚","🦜","🦢","🦩","🕊️","🐇","🦝","🦨","🦡","🦦","🦥","🐁","🐀","🐿","🦔",
 "🐉","🐲","🪐","💫","🌪","🌈","📠","📺"]
 
     if rng.random() > 0.5:
@@ -61,6 +61,7 @@ class Player(NamedTuple):
     id: str
     stlats: PlayerStlats 
     emoji:str = "🏌️"
+    modifications:list = []
 
     def driving_rating(self): # "Driving": hitting, and driving a kart
         rating_number = (self.stlats.musclitude + self.stlats.finesse + self.stlats.tofu)*5/3
@@ -78,32 +79,14 @@ class Player(NamedTuple):
         rating_number = (self.stlats.wiggle + self.stlats.polkadottedness) * 5 #means nothing for now
         return format_stlat_display(rating_number)
 
+    def modifications_string(self):
+        if len(self.modifications) == 0:
+            return ""
+        else:
+            return "**Modifications**:\n" + ", ".join(self.modifications)
 
 
-# Easter egg: polkadot has max stats
-known_players = {
-    "Polkadot Patterson": Player(name="Polkadot Patterson", id=1, stlats=PlayerStlats(
-        stance="Squiddish",
-        fav_tea= "Iced",
-        nyoomability = 1.5,
-        tofu=1, # unused
-        wiggle=1, # unused
-        ritualism=1, # unused
-        musclitude=1,
-        finesse=1,
-        needlethreadableness=1,
-        polkadottedness=1,  
-        left_handedness= 0),emoji="😅")
-}
-known_players["Alto"] = known_players["Polkadot Patterson"]
 
-def get_player_from_name(name):
-    # generate a player from their name with random stats
-    # ...or if they're polkadot, return a maxed person
-    if name.title() in known_players:
-        return known_players[name.title()]
-    else:
-        return generate_random_player_from_name(name)
 
 
 def generate_random_player_from_name(name="Random Player", emoji="🏌️"):
@@ -135,8 +118,6 @@ def generate_random_stlats_from_name(name="Random Player"):
     rng = random.Random(name) #seed with name
 
     return PlayerStlats(
-        stance= random.choice(["Tricky","Flashy","Aggro","Tanky","Twitchy","Powerful","Wibble","Wobble","Reverse","Feint","Electric","Spicy ","Pomegranate ","Explosive","Cottagecore","Corecore","Hardcore","Softcore","Hand to Hand","Lefty","Pop-Punk","DPS","Repose"]),
-        fav_tea= random.choice(["Iced","Boba","White","Green","Oolong","Pu'erh","Chai","Milk","Neon","Sweet","Void","Tea? ","Caramel","Lightspeed","Time-traveling","Bloody","Black","Miso","Concrete","Hard-boiled egg","Hot Chocolate","Bubble"]),
         nyoomability = max(rng.gauss(0,0.3),1.4),
         tofu=           rng.random(), # unused
         wiggle=         rng.random(), # unused
@@ -144,8 +125,39 @@ def generate_random_stlats_from_name(name="Random Player"):
         musclitude=     rng.random(),
         finesse=        rng.random(),
         needlethreadableness=   rng.random(), 
-        left_handedness=        rng.gauss(0,0.3) #how often shots are biased to the left or right of what you want
+        left_handedness=        rng.gauss(0,0.3), #how often shots are biased to the left or right of what you want
+        stance= rng.choice(["Tricky","Flashy","Aggro","Tanky","Twitchy","Powerful","Wibble","Wobble","Reverse","Feint","Electric","Spicy ","Pomegranate ","Explosive","Cottagecore","Corecore","Hardcore","Softcore","Hand to Hand","Lefty","Pop-Punk","DPS","Repose"]),
+        fav_tea= rng.choice(["Iced","Boba","White","Green","Oolong","Pu'erh","Chai","Milk","Neon","Sweet","Void","Tea? ","Caramel","Lightspeed","Time-traveling","Bloody","Black","Miso","Concrete","Hard-boiled egg","Hot Chocolate","Bubble"]),
     )
+
+
+
+# Easter egg: polkadot has max stats
+known_players = {
+    "Polkadot Patterson": Player(name="Polkadot Patterson", id=1, stlats=PlayerStlats(
+        stance="Squiddish",
+        fav_tea= "Iced",
+        nyoomability = 1.5,
+        tofu=1, # unused
+        wiggle=1, # unused
+        ritualism=1, # unused
+        musclitude=1,
+        finesse=1,
+        needlethreadableness=1,
+        polkadottedness=1,  
+        left_handedness= 0),emoji="😅"),
+    "Simulacrum": Player(name="Simulacrum", id=2,emoji="🐂",stlats=generate_random_stlats_from_name("Simulacrum"), modifications=["Ǫ̷͍̺̘͕̼̣͔̮̤̮̫͓̜͊͆̈́̈̉͌́̈̌͠ͅŭ̷̟̦̹͇̮͚̦̱̹̖̲̟̻͈̳͚̰̀̎͆̌̀t̴̨̨̹͇̬̠̤̳̘̟̩̜̻̳͓́̀͌̍̌","😱"]),
+    "Solar Dies": Player(name="Solar Dies", id=3,emoji="🦞",stlats=generate_random_stlats_from_name("Solar Dies"), modifications=["Ǫ̷͍̺̘͕̼̣͔̮̤̮̫͓̜͊͆̈́̈̉͌́̈̌͠ͅŭ̷̟̦̹͇̮͚̦̱̹̖̲̟̻͈̳͚̰̀̎͆̌̀t̴̨̨̹͇̬̠̤̳̘̟̩̜̻̳͓́̀͌̍̌"]),
+}
+known_players["Alto"] = known_players["Polkadot Patterson"]
+
+def get_player_from_name(name):
+    # generate a player from their name with random stats
+    # ...or if they're polkadot, return a maxed person
+    if name.title() in known_players:
+        return known_players[name.title()]
+    else:
+        return generate_random_player_from_name(name)
 
 def format_stlat_display(starcount: float):
     num_stars = math.floor(starcount)
