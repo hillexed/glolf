9b259f61ad9d0dcdcacfdbb9b3396d100ead7e5b
hillexed <hillexed@email.com>
2021-04-27 23:06:42 -0400

    Add auto-updater

--- bot.py
+++ bot.py
@@ -19,10 +19,8 @@ if len(sys.argv) > 1:
         print("Debug mode on!")
         prefix = 'd' + prefix
 
-client = discord.Client()
-@client.event
-async def on_ready():
-    print("The bot is ready!")
+
+
 
 async def newglolfgame(channel, glolfer_names, header="", turns=60):
     # start a round of glolf and return the winning player's name
@@ -48,6 +46,11 @@ async def newglolfgame(channel, glolfer_names, header="", turns=60):
 
 async def glolfcommand(message):
     # parse a glolf command
+
+    global users_with_games_active
+    if message.author in users_with_games_active and not debug:
+        return await message.channel.send("To avoid lag, please wait for your current game to finish before starting another.")
+
     arguments = message.content.split("\n") #first line has "!glolf" on it
     glolfer_names = []
     if len(arguments) > 1: # 0 players is fine
@@ -56,12 +59,22 @@ async def glolfcommand(message):
             await message.channel.send("It's too dangerous to glolf alone. Bring an opponent.")
             return 
 
+
+        if message.author in users_with_games_active:
+            return await message.channel.send("To avoid lag, please wait for your current game to finish before starting another.")
+
+    users_with_games_active.append(message.author)
     await newglolfgame(message.channel, glolfer_names)
+    users_with_games_active.remove(message.author)
 
 def biggest_power_of_two_less_than(n):
     return 2 ** math.floor(math.log2(n))
 
 async def one_v_one_glolftourney(message):
+
+    global users_with_games_active
+    if message.author in users_with_games_active:
+        return await message.channel.send("To avoid lag, please wait for your current game to finish before starting another.")
 
     arguments = message.content.split("\n") #first line has "!glolf" on it
     glolfer_names = []
@@ -75,21 +88,14 @@ async def one_v_one_glolftourney(message):
         await message.channel.send("To use, please specify a list of competitors, on one line each")
         return
 
-    '''
-    num_people_to_eliminate_first_round = num_players - biggest_power_of_two_less_than(n)
-
-    for i in range(num_people_to_eliminate_first_round):
-        await asyncio.sleep(2)
-        await newglolfgame(message.channel, glolfer_names)
-        '''
-
-    # is a power of two
+    # ensure # of entrants is a power of two
     if not len(glolfer_names) == biggest_power_of_two_less_than(len(glolfer_names)):
         await message.channel.send(f"I need a power-of-two number of people. You have {len(glolfer_names)}")
         return
 
     
     await message.channel.send(f"{len(glolfer_names)}-person tournament starting...")
+    users_with_games_active.append(message.author)
 
     round_num = 0
     while len(glolfer_names) > 1:
@@ -117,6 +123,7 @@ async def one_v_one_glolftourney(message):
         if len(move_onto_next_round) > 1:
             await message.channel.send(f"{len(move_onto_next_round)} contestants move on: {', '.join(move_onto_next_round)}")
     
+    users_with_games_active.remove(message.author)
     await message.channel.send(f"{glolfer_names[0]} wins the tournament!")
 
 async def get_glolfer_stats(message):
@@ -144,22 +151,69 @@ Stance: **{newplayer.stlats.stance}**
             await message.add_reaction('⚠️')
             raise e
 
+def user_is_admin(message):
+    if "ADMIN_IDS" in config:
+        print(config["ADMIN_IDS"].strip().split(","))
+        if str(message.author.id) in config["ADMIN_IDS"].strip().split(","):
+            return True
+    return False
+
+
+users_with_games_active = []
+
+
+
+
+
+
+client = discord.Client()
+@client.event
+async def on_ready():
+    print("The bot is ready!")
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user or message.webhook_id is not None:
         return
+
     if message.content.startswith(prefix + "glolf"):
         print("glolf detected")
-        await newglolfgame(message)
+        await glolfcommand(message)
 
     if message.content.startswith(prefix + "glolfer"):
         await get_glolfer_stats(message)
 
     if message.content.startswith(prefix + "tourney"):
+
         if message.content.startswith(prefix + "tourney 1v1"):
+            users_with_games_active.append(message.author)
             await one_v_one_glolftourney(message)
+            users_with_games_active.remove(message.author)
         else:
             await message.channel.send("The llawn only allows 1v1 tourneys right now. try "+prefix+"tourney 1v1")
+
+    if message.content.startswith(prefix + "discordid"):
+        print(message.author.id) # you should only be able to access this if you're an admin
+
+    # 'forceupdate' auto-updater
+    if user_is_admin(message) and message.content.startswith(prefix + "forceupdate"):
+        if "yes" not in message.content:
+            return await message.channel.send("Are you sure? If so add 'yes' to the end of your message") 
+        import subprocess
+        output = subprocess.check_output(["git", "stash"])
+        await message.channel.send(output)
+        await asyncio.sleep(3)
+        output = subprocess.check_output(["git", "pull","--force"])
+        await message.channel.send(output)
+        await message.channel.send(">_< Looks good! Restart me whenever you're ready!")
+        print("Update complete! Now I need to be restarted!")
+
+    # forcequit command
+    if user_is_admin(message) and message.content.startswith(prefix + "forcequit"):
+        if "yes" not in message.content:
+            return await message.channel.send("Are you sure? If so add 'yes' to the end of your message")
+        exit()
 
 # now run the bot
 token = config["TOKEN"]
