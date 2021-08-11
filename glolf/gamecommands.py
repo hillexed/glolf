844ce7ac17a13ef154dae4778a52a7b69174f3cb
hillexed <hillexed@email.com>
2021-08-10 22:52:13 -0400

    Bold winners of bo3s

--- glolf/gamecommands.py
+++ glolf/gamecommands.py
@@ -63,11 +63,11 @@ async def best_of_n_glolfgame(message, glolfer_names, wins_required=3, max_turns
 
     if len(winning_names) > 1:
         random_winner = random.choice(winning_names)
-        await message.channel.send(f"{random_winner} wins the tiebreaking duel to win the series!")
+        await message.channel.send(f"**{random_winner} wins the tiebreaking duel to win the series!**")
         return random_winner
     else:
         winning_name = winning_names[0]
-        await message.channel.send(f"{winning_name} wins the series!")
+        await message.channel.send(f"**{winning_name} wins the series!**")
         return winning_name
 
 async def newglolfgame(message, glolfer_names, header=None, max_turns=60, is_tournament=False, debug=False):
